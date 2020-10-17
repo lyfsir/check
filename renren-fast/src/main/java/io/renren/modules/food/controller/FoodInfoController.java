0bc40f9172af8dd938061f12c43829986d93e1c0
lyf <2653155409@qq.com>
2020-10-18 00:22:00 +0800

    人人开源

---
+++ renren-fast/src/main/java/io/renren/modules/food/controller/FoodInfoController.java
@@ -0,0 +1,43 @@
+package io.renren.modules.food.controller;
+
+import io.renren.common.utils.R;
+import io.renren.fegin.FoodSortFegin;
+import io.renren.modules.food.vo.UpdatefoodVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author lyf
+ * @date 2020/8/4-18:39
+ */
+@RestController
+@RequestMapping("/food/info")
+public class FoodInfoController {
+    @Autowired
+    FoodSortFegin foodSortFegin;
+    @GetMapping("doget/info/{foodId}")
+    public R doget(@PathVariable Integer foodId){
+        R r = foodSortFegin.selects(foodId);
+        return R.ok().put("data",r.get("data"));
+    }
+
+    @PostMapping("save/food")
+    public R save(@RequestBody UpdatefoodVo updatefoodVo){
+
+        System.out.println("----testf---"+updatefoodVo.toString());
+        foodSortFegin.save(updatefoodVo);
+        return R.ok();
+    }
+
+    @DeleteMapping("del/food")
+    public R del(@RequestBody Integer[] ids){
+        foodSortFegin.deletes(ids);
+        return R.ok();
+    }
+
+    @DeleteMapping("delete/food")
+    public R delete(@RequestBody Integer[] ids){
+        foodSortFegin.del(ids);
+        return R.ok();
+    }
+}
